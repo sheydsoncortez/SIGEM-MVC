852e403a20340575dee45b6a86cfd72e265922fd
Emanuel Costa <emancos@gmail.com>
2018-10-17 20:04:48 -0300

    Modificação CREATE-DATABASE.sql e ADD gitignore

--- database/create-database.sql
+++ database/create-database.sql
@@ -6,7 +6,5 @@ CREATE DATABASE sigem
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
