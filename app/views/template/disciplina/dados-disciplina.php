9e9cbfa3349f6fd07e8ac29a35fae75e8e8d2b3a
Sheydson Carlos Santos Cortez <sheydsoncortez@gmail.com>
2018-10-29 11:00:44 -0300

    Disciplina

---
+++ app/views/template/disciplina/dados-disciplina.php
@@ -0,0 +1,23 @@
+<?php if (isset($_SESSION['disciplina'])){
+    $f = $_SESSION['disciplina'];
+    $date = new DateTime($f->datanasc); $nd = $date->format('d/m/Y');
+}?>
+
+<!-- CADASTRO DE DISCIPLINA -->
+
+<div class="form-group row">
+    <div class="col-sm-12">
+        <div class="row">
+            <label class="col-sm-2 form-control-label"></label>
+            <div class="col-sm-8">
+                <input type="text" placeholder="Código da Disciplina" id="codigoDisciplina" name="codigoDisciplina"
+                       value="<?php isset($f) ? print($f->codigoDisciplina) : "" ?>"
+                       required oninvalid="this.setCustomValidity("Preencha o campo código da disciplina")"
+                oninput="setCustomValidity("")" class="form-control"/>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
