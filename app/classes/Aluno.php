b448ae027d680749e9d4031fc4372e68b9bb4ebd
Andre <andreggurgelufrn@gmail.com>
2018-10-31 16:59:10 -0300

    Separação da classe aluno com filiação.

--- app/classes/Aluno.php
+++ app/classes/Aluno.php
@@ -5,6 +5,7 @@ namespace app\classes;
 use app\core\Controller;
 use app\classes\Rg;
 use app\classes\DocumentosAluno;
+use app\classes\FiliacaoAluno;
 
 class Aluno{
 
@@ -23,20 +24,13 @@ class Aluno{
     private $codigoEscola;
 
     // Filiação
-    private $nomePaiAluno;
-    private $profissaoPai;
-    private $rgPaiAluno;
-    private $nomeMaeAluno;
-    private $profissaoMae;
-    private $rgMaeAluno;
+    private $filiacaoAluno;
 
     // Documentos Alunos
     private $documentosAluno;
 
     public function __construct(){
         $this->view = new \stdClass;
-        $this->rgPaiAluno = new Rg();
-        $this->rgMaeAluno = new Rg();
     }
 
     public function __get($atrib)
@@ -116,52 +110,12 @@ class Aluno{
         $this->statusAluno = $statusAluno;
     }
 
-    public function getNomePaiAluno(){
-        return $this->nomePaiAluno;
+    public function getFiliacao(){
+        return $this->filiacaoAluno;
     }
 
-    public function setNomePaiAluno($nomePaiAluno){
-        $this->nomePaiAluno = $nomePaiAluno;
-    }
-
-    public function getProfissaoPaiAluno(){
-        return $this->profissaoPai;
-    }
-
-    public function setProfissaoPaiAluno($profissaoPai){
-        $this->profissaoPai = $profissaoPai;
-    }
-
-    public function getRgPaiAluno(){
-        return $this->rgPaiAluno;
-    }
-
-    public function setRgPaiAluno(Rg $rgPaiAluno){
-        $this->rgPaiAluno = $rgPaiAluno;
-    }
-
-    public function getNomeMaeAluno(){
-        return $this->nomeMaeAluno;
-    }
-
-    public function setNomeMaeAluno($nomeMaeAluno){
-        $this->nomeMaeAluno = $nomeMaeAluno;
-    }
-
-    public function getProfissaoMaeAluno(){
-        return $this->profissaoMae;
-    }
-
-    public function setProfissaoMaeAluno($profissaoMae){
-        $this->profissaoMae = $profissaoMae;
-    }
-
-    public function getRgMaeAluno(){
-        return $this->rgMaeAluno;
-    }
-
-    public function setRgMaeAluno(Rg $rgMaeAluno){
-        $this->rgMaeAluno = $rgMaeAluno;
+    public function setFiliacao(FiliacaoAluno $filiacaoAluno){
+        $this->filiacaoAluno = $filiacaoAluno;
     }
 
     public function getDocumentosAluno(){
