a1bc9f0a991f14eddba31794013f3b0b2bc3af64
Michael Angelo <mikmichel@gmail.com>
2018-10-31 19:24:20 -0300

    Atualizando o script do banco de dados, inserindo a tabela escola

--- database/sigem.sql
+++ database/sigem.sql
@@ -1,10 +1,12 @@
+
+
 CREATE TABLE "public".ctps ( 
 	numero               varchar(20)  NOT NULL ,
 	serie                varchar(20)  NOT NULL ,
 	CONSTRAINT pk_carteira_profissional_numero_car PRIMARY KEY ( numero )
  );
 
-CREATE TABLE "public".disciplina (
+CREATE TABLE "public".disciplina ( 
 	codigo               bigint  NOT NULL ,
 	nome                 varchar(50)  NOT NULL ,
 	professor            integer  NOT NULL ,
@@ -13,7 +15,7 @@ CREATE TABLE "public".disciplina (
 	CONSTRAINT pk_disciplina_codigo PRIMARY KEY ( codigo )
  );
 
-CREATE TABLE "public".endereco (
+CREATE TABLE "public".endereco ( 
 	codigo               varchar(16)  NOT NULL ,
 	cep                  varchar(10)  NOT NULL ,
 	cidade               varchar(100)  NOT NULL ,
@@ -24,14 +26,25 @@ CREATE TABLE "public".endereco (
 	CONSTRAINT pk_endereco_codigo_end PRIMARY KEY ( codigo )
  );
 
-CREATE TABLE "public".reservista (
+CREATE TABLE "public".escola ( 
+	codigo               integer  NOT NULL ,
+	nome                 varchar(255)  NOT NULL ,
+	telefone             varchar(16)  NOT NULL ,
+	email                varchar(150)  NOT NULL ,
+	endereco             varchar(16)   ,
+	CONSTRAINT pk_escola_codigo PRIMARY KEY ( codigo )
+ );
+
+CREATE INDEX idx_escola_endereco ON "public".escola ( endereco );
+
+CREATE TABLE "public".reservista ( 
 	numero               varchar(15)  NOT NULL ,
 	categoria            varchar(20)  NOT NULL ,
 	serie                char(2)  NOT NULL ,
 	CONSTRAINT pk_reservista_numero_res PRIMARY KEY ( numero )
  );
 
-CREATE TABLE "public".rg (
+CREATE TABLE "public".rg ( 
 	numero               varchar(16)  NOT NULL ,
 	orgaoexp             varchar(16)  NOT NULL ,
 	dataexp              date  NOT NULL ,
@@ -39,14 +52,14 @@ CREATE TABLE "public".rg (
 	CONSTRAINT pk_rg_numero_rg PRIMARY KEY ( numero )
  );
 
-CREATE TABLE "public".tituloeleitor (
+CREATE TABLE "public".tituloeleitor ( 
 	numero               varchar(20)  NOT NULL ,
 	zona                 integer  NOT NULL ,
 	secao                integer  NOT NULL ,
 	CONSTRAINT pk_titulo_eleitoral_numero_tit PRIMARY KEY ( numero )
  );
 
-CREATE TABLE "public".funcionario (
+CREATE TABLE "public".funcionario ( 
 	cpf                  varchar(16)  NOT NULL ,
 	nome                 varchar(150)  NOT NULL ,
 	datanasc             date  NOT NULL ,
@@ -97,6 +110,8 @@ COMMENT ON COLUMN "public".funcionario.tituloeleitor IS 'Número do título elei
 
 COMMENT ON COLUMN "public".funcionario.reservista IS 'Número da reservista - Chave estrangeira para entidade Reservista';
 
+ALTER TABLE "public".escola ADD CONSTRAINT fk_escola_endereco FOREIGN KEY ( endereco ) REFERENCES "public".endereco( codigo );
+
 ALTER TABLE "public".funcionario ADD CONSTRAINT fk_funcionario_carteiraprof FOREIGN KEY ( ctps ) REFERENCES "public".ctps( numero );
 
 ALTER TABLE "public".funcionario ADD CONSTRAINT fk_funcionario_endereco FOREIGN KEY ( endereco ) REFERENCES "public".endereco( codigo );
@@ -107,28 +122,34 @@ ALTER TABLE "public".funcionario ADD CONSTRAINT fk_funcionario_rg FOREIGN KEY ( 
 
 ALTER TABLE "public".funcionario ADD CONSTRAINT fk_funcionario_tituloeleitoral FOREIGN KEY ( tituloeleitor ) REFERENCES "public".tituloeleitor( numero );
 
-INSERT INTO "public".ctps( numero, serie ) VALUES ( '000000', '0000' );
-INSERT INTO "public".ctps( numero, serie ) VALUES ( '34535656', '4554' );
-INSERT INTO "public".ctps( numero, serie ) VALUES ( '987654321', '00012' );
+INSERT INTO "public".ctps( numero, serie ) VALUES ( '000000', '0000' ); 
+INSERT INTO "public".ctps( numero, serie ) VALUES ( '34535656', '4554' ); 
+INSERT INTO "public".ctps( numero, serie ) VALUES ( '987654321', '00012' ); 
+INSERT INTO "public".ctps( numero, serie ) VALUES ( '', '' ); 
 
-INSERT INTO "public".disciplina( codigo, nome, professor, turma, serie ) VALUES ( 3, 'Gestão de Projetos', 2147483647, 144, '22' );
-INSERT INTO "public".disciplina( codigo, nome, professor, turma, serie ) VALUES ( 4, 'PV', 0, 2345678, '123' );
+INSERT INTO "public".disciplina( codigo, nome, professor, turma, serie ) VALUES ( 3, 'Gestão de Projetos', 2147483647, 144, '22' ); 
+INSERT INTO "public".disciplina( codigo, nome, professor, turma, serie ) VALUES ( 4, 'PV', 0, 2345678, '123' ); 
 
-INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '000.000.000-00', '59300-000', 'Caicó', 'Av. Coronel Martiniano', 'S/N', 'Centro', 'RN' );
-INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '123.123.123-12', '59010-015', 'Natal', 'Rua Miramar', '10', 'Praia do Meio', '' );
-INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '026.898.294-52', '59300-000', 'Caicó', 'Rua Esperidião Elói de Medeiros', '131', 'João XXIII', 'RN' );
+INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '000.000.000-00', '59300-000', 'Caicó', 'Av. Coronel Martiniano', 'S/N', 'Centro', 'RN' ); 
+INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '123.123.123-12', '59010-015', 'Natal', 'Rua Miramar', '10', 'Praia do Meio', '' ); 
+INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '026.898.294-52', '59300-000', 'Caicó', 'Rua Esperidião Elói de Medeiros', '131', 'João XXIII', 'RN' ); 
+INSERT INTO "public".endereco( codigo, cep, cidade, logradouro, numero, bairro, estado ) VALUES ( '', '', '', '', '', '', '' ); 
 
-INSERT INTO "public".reservista( numero, categoria, serie ) VALUES ( '87654321', '8777', 'M ' );
-INSERT INTO "public".reservista( numero, categoria, serie ) VALUES ( '000000000', '000000000', '0 ' );
+INSERT INTO "public".escola( codigo, nome, telefone, email, endereco ) VALUES ( 1, 'teste', '(00)00000-0000', 'escola@teste.com', '000.000.000-00' ); 
+INSERT INTO "public".escola( codigo, nome, telefone, email, endereco ) VALUES ( 2, 'Escola 2', '(11)11111-1111', 'escola2t@teste.com', '123.123.123-12' ); 
+INSERT INTO "public".escola( codigo, nome, telefone, email, endereco ) VALUES ( 3, 'Escola 3', '(22)22222-2222', 'escola3@teste.com', '026.898.294-52' ); 
 
-INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '0000000', 'SSP', '2018-01-01', 'AC ' );
-INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '7344534543', 'ITEP', '2002-10-20', 'RN ' );
-INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '1669132', 'SSP', '2018-10-23', 'RN ' );
+INSERT INTO "public".reservista( numero, categoria, serie ) VALUES ( '87654321', '8777', 'M ' ); 
+INSERT INTO "public".reservista( numero, categoria, serie ) VALUES ( '000000000', '000000000', '0 ' ); 
 
-INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '00000000', 0, 0 );
-INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '09987654', 8, 45 );
-INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '161743616143', 25, 12 );
+INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '0000000', 'SSP', '2018-01-01', 'AC ' ); 
+INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '7344534543', 'ITEP', '2002-10-20', 'RN ' ); 
+INSERT INTO "public".rg( numero, orgaoexp, dataexp, ufexp ) VALUES ( '1669132', 'SSP', '2018-10-23', 'RN ' ); 
 
-INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '000.000.000-00', 'Administrador', '2018-01-01', 'Sem cidade', 'AC ', 'Sem pai', 'Sem mãe', 'M  ', '0', '(00)00000-0000', 'admin@sigem.com', '000000000', 'admin', '2018-01-01', 'Ensino superior', 'Sem formação', 2018, 'Administrador', 'Administrador do sistema', '000.000.000-00', '000000', '0000000', '00000000', '000000000', '21232f297a57a5a743894a0e4a801fc3', '24032239', '0' );
-INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '123.123.123-12', 'Emanuel Costa', '1986-11-30', 'Caicó', 'RN ', 'Manoel', 'Maria', 'M  ', 'Solteiro(a)', '(84)90000-0000', 'emancos@gmail.com', '123.12312.31.2', '12121212', '2013-06-26', 'Ensino médio', '-', 2004, 'Auxiliar de Serviços Gerais', 'ASG', '123.123.123-12', '34535656', '7344534543', '09987654', '87654321', 'd89ca9451043b1b65f4a70646c9b24c6', '24032239', '1' );
-INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '026.898.294-52', 'Sueide Maria de Medeiros', '1977-05-16', 'Caicó', 'RN ', 'José Pereira Dantas', 'Otaciana Otacília de Medeiros', 'F  ', 'Solteiro(a)', '(84)00417-3564', 'sememail@email.email', '123.11112.22.2', '12.173/1', '2018-10-23', 'Ensino médio', '-', 2004, 'Auxiliar de Serviços Gerais', 'ASG', '026.898.294-52', '987654321', '1669132', '161743616143', '000000000', '27d81fcd15e2dcf8ada0b82ee9b1a3e5', '24032239', '1' );
+INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '00000000', 0, 0 ); 
+INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '09987654', 8, 45 ); 
+INSERT INTO "public".tituloeleitor( numero, zona, secao ) VALUES ( '161743616143', 25, 12 ); 
+
+INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '000.000.000-00', 'Administrador', '2018-01-01', 'Sem cidade', 'AC ', 'Sem pai', 'Sem mãe', 'M  ', '0', '(00)00000-0000', 'admin@sigem.com', '000000000', 'admin', '2018-01-01', 'Ensino superior', 'Sem formação', 2018, 'Administrador', 'Administrador do sistema', '000.000.000-00', '000000', '0000000', '00000000', '000000000', '21232f297a57a5a743894a0e4a801fc3', '24032239', '0' ); 
+INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '123.123.123-12', 'Emanuel Costa', '1986-11-30', 'Caicó', 'RN ', 'Manoel', 'Maria', 'M  ', 'Solteiro(a)', '(84)90000-0000', 'emancos@gmail.com', '123.12312.31.2', '12121212', '2013-06-26', 'Ensino médio', '-', 2004, 'Auxiliar de Serviços Gerais', 'ASG', '123.123.123-12', '34535656', '7344534543', '09987654', '87654321', 'd89ca9451043b1b65f4a70646c9b24c6', '24032239', '1' ); 
+INSERT INTO "public".funcionario( cpf, nome, datanasc, cidadenasc, estadonasc, nomepai, nomemae, sexo, estadocivil, telefone, email, pispasep, matricula, dataadmissao, escolaridade, formacaoacademica, anoconclusao, cargo, funcao, endereco, ctps, rg, tituloeleitor, reservista, senha, escola, ativo ) VALUES ( '026.898.294-52', 'Sueide Maria de Medeiros', '1977-05-16', 'Caicó', 'RN ', 'José Pereira Dantas', 'Otaciana Otacília de Medeiros', 'F  ', 'Solteiro(a)', '(84)00417-3564', 'sememail@email.email', '123.11112.22.2', '12.173/1', '2018-10-23', 'Ensino médio', '-', 2004, 'Auxiliar de Serviços Gerais', 'ASG', '026.898.294-52', '987654321', '1669132', '161743616143', '000000000', '27d81fcd15e2dcf8ada0b82ee9b1a3e5', '24032239', '1' ); 
