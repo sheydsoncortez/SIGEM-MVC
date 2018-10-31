243df524699123d1ced48c5085f01b5fbb96ca2c
Andre <andreggurgelufrn@gmail.com>
2018-10-30 21:10:14 -0300

    Correções no controller aluno.

--- app/controllers/AlunoController.php
+++ app/controllers/AlunoController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\models\AlunoModel;
 use app\classes\Rg;
 use app\classes\DocumentosAluno;
 
@@ -15,16 +16,30 @@ class AlunoController extends Controller{
         $this->load("admin");
     }
 
-    public function cadastrar(){
+    public function cadastrar($page){
 
         $dados["view"] = "template/form-aluno";
-        $dados["modal"] = "template/professor/modal-professor.php";
         $dados["page"] = $page;
         $dados["voltar"] = "";
         $dados["proximo"] = "";
         $alunos = new AlunoModel();
-        $dados['link'] = "funcionario/cadastrar/".$page;
+        $dados['link'] = "aluno/cadastrar/".$page;
         $dados['breadcrumbl1'] = "aluno";
         $dados['breadcrumbl2'] = "cadastrar";
+
+        switch($page){
+            case '1':
+                $dados["titulo"] = "DADOS DO ALUNO";
+                $dados["paginator"] = "aluno/dados-aluno.php";
+                $dados["active"] = array("active", "", "", "", "");
+                $dados["disabled"] = array("disabled", "", "disabled", "disabled", "disabled", "");
+                $dados["voltar"] = "1";
+                $dados["proximo"] = "2";
+                $dados["page"] = $page;
+                break;
+        }
+
+        $this->load("admin", $dados);
+        unset($dados);
     }
 }
