48dec45cb4fd3a813ec2c94717c1293bdf11c358
Michael Angelo <mikmichel@gmail.com>
2018-11-07 21:16:23 -0300

    Atualizando dados-Escola

--- app/views/template/escola/dados-escola.php
+++ app/views/template/escola/dados-escola.php
@@ -7,7 +7,7 @@
  */
 
  if (isset($_SESSION['escola'])){
-    $f = $_SESSION['escola'];
+    $e = $_SESSION['escola'];
 }?>
 
 <!-- CÓDIGO DA ESCOLA -->
@@ -17,7 +17,7 @@
             <label class="col-sm-2 form-control-label"></label>
             <div class="col-sm-8">
                 <input type="text" placeholder="Código da Escola" id="codigoEscola" name="codigo"
-                       value="<?php isset($f) ? print($f->codigo) : "" ?>"
+                       value="<?php isset($e) ? print($e->codigo) : "" ?>"
                        required oninvalid="this.setCustomValidity("Preencha o campo código da Escola")"
                 oninput="setCustomValidity("")" class="form-control"/>
             </div>
@@ -32,7 +32,7 @@
             <label class="col-sm-2 form-control-label"></label>
             <div class="col-sm-8">
                 <input type="text" placeholder="Nome da Escola" id="nomeEscola" name="nome"
-                       value="<?php isset($f) ? print($f->nome) : "" ?>"
+                       value="<?php isset($e) ? print($e->nome) : "" ?>"
                        required oninvalid="this.setCustomValidity("Preencha o campo nome da disciplina")"
                 oninput="setCustomValidity("")" class="form-control"/>
             </div>
@@ -49,14 +49,14 @@
             <div class="col-sm-3">
                 <input type="text" placeholder="Telefone: (00)00000-0000"
                        class="form-control" id="telefone" name="telefone" required
-                       value="<?php isset($f) ? print($f->telefone) : ""?>"
+                       value="<?php isset($e) ? print($e->telefone) : ""?>"
                        oninvalid="this.setCustomValidity('Informe um número para contato')"
                        oninput="setCustomValidity('')"/>
             </div>
             <!-- EMAIL DA ESCOLA -->
             <div class="col-sm-4">
                 <input type="email" placeholder="Email: login@provedor.com" class="form-control"
-                       value="<?php isset($f) ? print($f->email) : ""?>"
+                       value="<?php isset($e) ? print($e->email) : ""?>"
                        required oninvalid="this.setCustomValidity('Insira um email válido')"
                        oninput="setCustomValidity('')" name="email"/>
             </div>
@@ -64,4 +64,4 @@
     </div>
 
 </div>
-<?php unset($f) ?>
+<?php unset($e) ?>
